81c432ea4fd81ef84a59ea7c88c490ecd2dd8879
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-25 02:34:54 +0800

    Get external IP address and advertise

--- appengine/main.go
+++ appengine/main.go
@@ -5,11 +5,12 @@ import (
 	//// "io/ioutil"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/mitchellh/cli"
+	"cloud.google.com/go/compute/metadata"
 	"github.com/hashicorp/consul/command"
 	"github.com/hashicorp/consul/lib"
-	"github.com/mitchellh/cli"
-	"strings"
 	"github.com/hashicorp/consul/ipaddr"
 )
 
@@ -27,24 +28,37 @@ func realMain() int {
 	// Previously: log.SetOutput(ioutil.Discard)
 	// Previously: args := os.Args[1:]
 
-	//  Get IP address.
+	//  Get Internal and External IP addresses.
 	iplist, err := ipaddr.GetPrivateIPv4(); if err != nil { log.Panic(err) }
-	log.Printf("iplist: %v", iplist)
-	host := iplist[0].String()
+	internalIP := iplist[0].String()
+	externalIP, err := metadata.ExternalIP();
+	if err != nil {
+		log.Printf("*** Unable to get External IP")
+		externalIP = internalIP
+	} else {
+		log.Printf("*** External IP: %s", externalIP)
+	}
+	log.Printf("Internal IP: %v", iplist)
 
 	//  Set node name
 	// log.Println(strings.Join(os.Environ(), "\n"))
 	node := strings.Replace(os.Getenv("GCLOUD_PROJECT"), "unabiz-", "", -1)
 
 	//  Bootstrap first agent
-	// cmd := "agent -bootstrap-expect=1"
-	//  Subsequent agents will join
+	//  cmd := "agent -bootstrap-expect=1"
+
+	//  Subsequent agents:
 	cmd := "agent"
 	para := ""
 
+	//  Subsequent agents will join the first agent using this command line:
+	//  	./consul join -wan -http-addr=http://Luppys-MacBook-Pro.local:8080 x.x.x.x
+	//  where x.x.x.x is the External IP address obtained from App Engine -> Instances -> VM IP
+
 	cmdline := cmd + " " +
 		"-node=" + node + " " +
-		"-client=" + host + " -http-port=8080 " +
+		"-client=" + internalIP + " -http-port=8080 " + //  Needed for App Engine health check
+		"-advertise-wan=" + externalIP + " " +  //  Ports 8301, 8302 use a different external IP address
 		"-server -ui -enable-script-checks=true " +
 		"-data-dir=./" + node + "/data " +
 		"-config-dir=./" + node + "/conf"
